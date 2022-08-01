b5f32d490957d815c9a8aa72d85a2b177a52e92c
Francastellano1 <francastellano89@gmail.com>
2022-07-31 23:21:14 -0300

    modif

--- src/main/java/com/portafolio/fc/Controller/CEducacion.java
+++ src/main/java/com/portafolio/fc/Controller/CEducacion.java
@@ -20,12 +20,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/educacion")
-@CrossOrigin(origins = "https://castellanoap.herokuapp.com")
+@CrossOrigin(origins = "https://castellanoap.herokuapp.com/educacion")
 
 public class CEducacion {
     @Autowired
--- src/main/java/com/portafolio/fc/Controller/CSkill.java
+++ src/main/java/com/portafolio/fc/Controller/CSkill.java
@@ -16,12 +16,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://castellanoap.herokuapp.com")
-@RequestMapping ("/skill")
+@CrossOrigin(origins = "https://castellanoap.herokuapp.com/skill")
+
 
 public class CSkill {
     @Autowired
