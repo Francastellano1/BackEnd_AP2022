2641e7ec4e89d13ec576cbec2326ab26cdccb68a
Francastellano1 <francastellano89@gmail.com>
2022-07-31 23:24:49 -0300

    d

--- src/main/java/com/portafolio/fc/Controller/CSkill.java
+++ src/main/java/com/portafolio/fc/Controller/CSkill.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://castellanoap.herokuapp.com/skill")
+@CrossOrigin(origins = "https://castellanoap-e8d94.web.app/skill")
 
 
 public class CSkill {
